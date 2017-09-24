631c306545b323ed8a0a7ef7bb5af7ca938d0e82
Matan Rubin <matan.rubin@solaredge.com>
2017-09-24 13:02:10 +0300

    Add equals, hashCode and toString methods

--- src/main/java/com/solaredge/spring5webapp/model/Author.java
+++ src/main/java/com/solaredge/spring5webapp/model/Author.java
@@ -50,4 +50,19 @@ public class Author {
                 ", books=" + books +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
