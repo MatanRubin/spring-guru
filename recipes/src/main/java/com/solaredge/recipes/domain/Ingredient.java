3e975356e06d52fc58eabea37876e03086feff7a
Matan Rubin <matan.rubin@solaredge.com>
2017-10-01 14:03:40 +0300

    Add an Ingredient object and define 1:N relationship with Recipe

---
+++ recipes/src/main/java/com/solaredge/recipes/domain/Ingredient.java
@@ -0,0 +1,38 @@
+package com.solaredge.recipes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+}
