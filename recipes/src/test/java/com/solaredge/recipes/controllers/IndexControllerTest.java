3dca16ba5f53335efc2e1b55275c2966198f08cf
Matan Rubin <matanaloni@gmail.com>
2017-10-09 12:05:40 +0300

    Add a JUnit + Mockito based test for index controller

---
+++ recipes/src/test/java/com/solaredge/recipes/controllers/IndexControllerTest.java
@@ -0,0 +1,61 @@
+package com.solaredge.recipes.controllers;
+
+import com.solaredge.recipes.domain.Recipe;
+import com.solaredge.recipes.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class IndexControllerTest {
+
+    private IndexController indexController;
+
+    @Mock
+    private RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
+        String viewName = indexController.getIndexPage(model);
+
+        // then
+        assertEquals("index", viewName);
+        verify(model, times(1)).addAttribute(Mockito.eq("recipes"), argumentCaptor.capture());
+        verify(recipeService, times(1)).getRecipes();
+
+        Set<Recipe> returnedRecipes = argumentCaptor.getValue();
+        assertEquals(recipes.size(), returnedRecipes.size());
+    }
+
+}
