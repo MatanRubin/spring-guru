62be5a5c8ae0eac7e1c2fa1e97a72a40a0b3835c
Matan Rubin <matanaloni@gmail.com>
2017-10-15 16:49:00 +0300

    Create initial module for REST API module

---
+++ spring5-mvc-rest/src/main/java/com/solaredge/spring5mvcrest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package com.solaredge.spring5mvcrest.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ spring5-mvc-rest/src/main/java/com/solaredge/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.solaredge.spring5mvcrest.repositories;
+
+import com.solaredge.spring5mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
